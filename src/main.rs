814d37b0ffad59e359eef35985602cb155d24840
RJD02 <dulangeraviraj@gmail.com>
2024-07-13 20:51:54 +0530

    Creating another function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
 
-fn main() {
+fn hello() {
     let x: i32 = 32;
     println!("Hello, world!");
     println!("Something new in master");
 }
+
+fn main() {}
 
 // hello copilot

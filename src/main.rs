96c004c6655360ddfc69494b61ca199686fb667a
RJD02 <dulangeraviraj@gmail.com>
2024-07-13 23:03:07 +0530

    Constants

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
 #![allow(unused)]
 
-struct BankAccount {
-    owner: String,
-    balance: f64,
-}
+const YOU_CAN_DECLARE_A_CONSTANT_HERE: i32 = 45;
 
-impl BankAccount {
-    fn withdraw(&mut self, amount: f64) {
-        println!(
-            "Withdrawing {} from account owned by {}",
-            amount, self.owner
-        );
-        self.balance -= amount;
-    }
-
-    fn check_balance(&self) {
-        println!(
-            "Account owned by {} has balance of {}",
-            self.owner, self.balance
-        );
-    }
-}
-
-// At any point a variable can have only one owner(mutable) or many immutable borrowers
-// But cannot have both at the same time
-// By default every variable in rust is immutable
 fn main() {
-    let mut account = BankAccount {
-        owner: "Raviraj".to_string(),
-        balance: 300.34,
-    };
-    // Immutable borrow to check the balance
-    account.check_balance();
-    // Mutable borrow to withdraw money
-    account.withdraw(150.45);
-    // check balance again
-    account.check_balance();
+    println!("Hello, world");
+    const Y: i8 = 5; // upper case
+                     // constant is immutable throughout it's lifecycle
+                     // you cannot mutate it
+                     // always have to provide type for it
 }

ef2bf86e07efbaafadb07471664ddcb30453a8f0
RJD02 <dulangeraviraj@gmail.com>
2024-07-13 20:58:51 +0530

    Each variable has it's own owner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 #![allow(unused)]
 
-fn main() {}
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+// Each variable has it's own owner
+fn main() {
+    let s1 = String::from("Rust");
+    let len = calculate_length(&s1);
+    println!("Length of {} is {}", s1, len);
+}
 
 // hello copilot

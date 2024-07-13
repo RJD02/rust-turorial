7ce8433b23f3df72239631a445adb4b2b0a1a5f7
RJD02 <dulangeraviraj@gmail.com>
2024-07-14 00:00:37 +0530

    Enums

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,18 @@
 #![allow(unused)]
 
+use core::fmt;
+
 fn main() {
-    // let age = 15;
-    // let result = if age > 10 { "allowed" } else { "not allowed" };
-    // println!("{result}")
-    // loop {
-    //     println!("Hello World");
-    // }
-    // let mut counter = 0;
-
-    // let result = loop {
-    //     counter += 1;
-
-    //     if counter == 10 {
-    //         break counter * 2;
-    //     }
-    // };
-    // println!("{result}");
-
-    // loop labels
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
-
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 2 {
-                break;
-            }
-
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-            count += 1;
-        }
+    // Enums
+    enum IpAddrKind {
+        V4(String),
+        V6(String),
     }
+
+    // struct IpAddr {
+    //     kind: IpAddrKind,
+    // }
+
+    let four = IpAddrKind::V4(String::from("127.0.0.1"));
+    let six = IpAddrKind::V6(String::from("::1"));
 }
